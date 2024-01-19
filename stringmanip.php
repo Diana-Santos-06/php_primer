d44d64f70c2af2397ae4e709f6fddace830450c8
Diana-Santos-06 <santosdiana96028@gmail.com>
2024-01-19 12:57:57 -0600

    stringmanipulation.php
    codigo donde se insertan frases y se pueden modificar

---
+++ stringmanip.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>PHP Primer  - String Manipulationg</title>
+</head>
+<body>
+    <?php 
+        $phrase1 = "student who came late";
+        $phrase2 = "in class, stand with Rock";
+        echo $phrase1;
+        echo $phrase2;
+    ?>
+</body>
+</html>
