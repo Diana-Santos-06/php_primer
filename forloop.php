cfd7d00efdfa4936a599616eef5ab7335d80b740
Diana-Santos-06 <santosdiana96028@gmail.com>
2024-01-19 10:49:04 -0600

    forlooop.php
    codigo que impime varias veces en la pantalla

---
+++ forloop.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>PHP Primer - For Loop</title>
+</head>
+<body>
+    <h1></h1> For Loops</h1>
+    <?php 
+        //For Loops
+     for($count = 0; $count < 20; $count++){
+        echo '<p>HELLO WORLD</p>';
+     }
+     for($count = 0; $count < 10; $count++){
+        echo "The Count is: $count</p>";
+     }
+        
+    ?>
+</body>
+</html>
