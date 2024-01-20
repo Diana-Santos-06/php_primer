383b460af2ce8bf55e71e6d786c62d55bf43f719
Diana-Santos-06 <santosdiana96028@gmail.com>
2024-01-20 14:46:02 -0600

    nombre en mayuscula
    se modifico para poder hacer que el nombre empiece con muyuscula

--- stringmanip.php
+++ stringmanip.php
@@ -9,7 +9,12 @@
     <?php 
         $phrase1 = "student who came late";
         $phrase2 = "in class, stand with Rock";
+        $name = "diana esther";
         echo $phrase1 . ",named Beto,  " . $phrase2;
+        echo '<br/>';
+
+        //String transformation
+        echo 'Uppercase first letter: ' . ucfirst($name).'<br/>';
 
         
     ?>
