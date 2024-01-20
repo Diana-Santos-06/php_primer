302cba8436030058b1ad8628be71e8a7d83d5416
Diana-Santos-06 <santosdiana96028@gmail.com>
2024-01-20 15:06:26 -0600

    mayusculas
    se agrego otra linea de codigo para que se pueda mostrar todo el nombre en mayusculas

--- stringmanip.php
+++ stringmanip.php
@@ -6,7 +6,9 @@
     <title>PHP Primer  - String Manipulationg</title>
 </head>
 <body>
+    <h1>PHP String Manipulation</h1>
     <?php 
+    //Concatenation of Strings
         $phrase1 = "student who came late";
         $phrase2 = "in class, stand with Rock";
         $name = "diana esther";
@@ -14,7 +16,9 @@
         echo '<br/>';
 
         //String transformation
-        echo 'Uppercase first letter: ' . ucfirst($name).'<br/>';
+        echo 'Uppercase first letter: ' . ucfirst($name). '<br/>';
+        echo 'Uppercase first letter of each word: ' . ucwords($name). '<br/>';
+        echo 'Upper case: '. strtoupper($name).'<br/>';
 
         
     ?>
