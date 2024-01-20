840fdfcb554e36479cf45e83146ddb8886a0fdfc
Diana-Santos-06 <santosdiana96028@gmail.com>
2024-01-20 14:30:47 -0600

    cambio de frase
    se agrega una frase para ver como funciona las comas con los espacios separados y como se ve en la pagina web

--- stringmanip.php
+++ stringmanip.php
@@ -9,8 +9,9 @@
     <?php 
         $phrase1 = "student who came late";
         $phrase2 = "in class, stand with Rock";
-        echo $phrase1;
-        echo $phrase2;
+        echo $phrase1 . ",named Beto,  " . $phrase2;
+
+        
     ?>
 </body>
 </html>
