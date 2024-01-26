4dade92a85271074ce196f39e8cec4fd58025384
Diana-Santos-06 <santosdiana96028@gmail.com>
2024-01-25 18:19:08 -0600

    change
    este avance es el que hace que a pesar del resultado siempre imprimira el valor que se asigno

--- functions.php
+++ functions.php
@@ -26,12 +26,21 @@
 
      function addFunction($num1, $num2){
         $sum = $num1 + $num2;
+        $num2 = $num2 + 1;
         echo     "The sum of     $num1 and $num2 is: $sum <br/>";
      }
+
+     function changeNum($num){
+        $num = $num + 10;
+     }
+
      $num = 500;
      addFunction(10, 20);
      addFunction(10, $num);
      addFunction('10', "50");
+
+     changeNum($num);
+     echo $num . '<br/>';
 
 
 
