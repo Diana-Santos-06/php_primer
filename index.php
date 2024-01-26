f5983a690036e351725b5893cbb6487728dc8f39
Diana-Santos-06 <santosdiana96028@gmail.com>
2024-01-25 18:41:18 -0600

    include

--- index.php
+++ index.php
@@ -1,25 +1,4 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>PHP Primer - Index</title>
-</head>
-<body>
-    <h2>Follow each link t the page example</h2>
-    <u1>
-        <li><a href="arrey.php">Simple Array and Printouts</a></li>
-        <li><a href="forloop.php">Simple For Loop</a></li>
-        <li><a href="whiledowhileloop.php">Simple While/Do While Loop</a></li>
-        <li><a href="ifstatement.php">Simple If Statement</a></li>
-        <li><a href="switchstatement.php">Simple Switch Statement</a></li>
-        <li><a href="stringmanip.php">Simple String Manipulation</a></li>
-        <li><a href="datetimemanip.php">Simple Date and Time Manipulation</a></li>
-        <li><a href="functions.php">Simple Functions</a></li>
-
-
-    </u1>
-        
+<?php include 'includes/header.php'?>
     <!--Basic HTML -->
     <h1>Hello HTML - PHP Primer</h1>
     <br/>
