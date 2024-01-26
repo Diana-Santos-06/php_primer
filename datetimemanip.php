9eb4de5cc65f29584584db38c4e40b806c762012
Diana-Santos-06 <santosdiana96028@gmail.com>
2024-01-25 18:40:16 -0600

    include

--- datetimemanip.php
+++ datetimemanip.php
@@ -1,11 +1,4 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>PHP - Primer - Date and Time Manipulation</title>
-</head>
-<body>
+<?php include 'includes/header.php'?>
     <h1>Date and Time Manipulation</h1>
     <?php
     $datenow = getdate();
